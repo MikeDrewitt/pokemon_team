1d73b01e3d5d41e1587f56140a007c01f9e76baa
Michael Drewitt <mikedrewitt@gmail.com>
2017-10-04 23:59:03 -0400

    playing with the API on the main controller

--- app/app.controller.js
+++ app/app.controller.js
@@ -1,13 +1,12 @@
 var GetPokemon = angular.module('GetPokemon', []);
 GetPokemon.controller('GetPokemon', function($scope, $http) {
 
-  console.log($scope);
-
   $scope.url = 'https://pokeapi.co/api/v1/pokedex/';
+  $scope.url2 = 'https://pokeapi.co/api/v2/pokemon/';
 
   // start with no pokes - probably not the best way to do this, would normally ping
   // an account info/ game info for a user. I don't feel like setting up users.
-  $scope.team  = [{name: 'bob'}, {}, {}, {}, {}, {}];
+  $scope.team  = [{}, {}, {}, {}, {}, {}];
   // populated from the API as to always be up to date.
   $scope.pokedex = [];
 
@@ -28,4 +27,19 @@ GetPokemon.controller('GetPokemon', function($scope, $http) {
   $scope.testing = function() {
     console.log($scope.team);
   }
+
+  $scope.results = null;
+  $scope.getDetails = function() {
+    // console.log(ctrl.url);
+    $http({
+      method: 'GET',
+      url: $scope.url2 + $scope.searchValue
+    }).then(function successCallback(response) {
+      // $scope.team.push(response);
+      $scope.results = response;
+      console.log(response);
+    }, function errorCallback(response) {
+      alert($scope.searchValue + " is not a pokemon!");
+    })
+  };
 });
